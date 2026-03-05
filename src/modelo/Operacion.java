5a39e6c734a581f097a11baa9eaecd5e46551192
gtapiero <giovanny.tapiero@pi.edu.co>
2026-03-05 14:50:31 -0500

    commit - Operacion .

--- src/modelo/Operacion.java
+++ src/modelo/Operacion.java
@@ -1,4 +1,32 @@
 package modelo;
 
 public class Operacion {
-}
+
+    private double numero1;
+    private double numero2;
+    private String operacion;
+    private double resultado;
+
+    public Operacion(double numero1, double numero2, String operacion, double resultado) {
+        this.numero1 = numero1;
+        this.numero2 = numero2;
+        this.operacion = operacion;
+        this.resultado = resultado;
+    }
+
+    public double getNumero1() {
+        return numero1;
+    }
+
+    public double getNumero2() {
+        return numero2;
+    }
+
+    public String getOperacion() {
+        return operacion;
+    }
+
+    public double getResultado() {
+        return resultado;
+    }
+}   
