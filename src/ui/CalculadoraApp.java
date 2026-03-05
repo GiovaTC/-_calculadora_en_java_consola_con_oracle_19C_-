53b185882248f627a0f37bd2a1aa0035a8c1510a
gtapiero <giovanny.tapiero@pi.edu.co>
2026-03-05 15:08:15 -0500

    commit - CalculadoraApp .

--- src/ui/CalculadoraApp.java
+++ src/ui/CalculadoraApp.java
@@ -1,17 +1,51 @@
 package ui;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class CalculadoraApp {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import servicio.CalculadoraServicio;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+import java.util.Scanner;
+
+public class CalculadoraApp {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        CalculadoraServicio calculadora = new CalculadoraServicio();
+
+        int opcion;
+
+        do {
+
+            System.out.println("\n===== CALCULADORA JAVA + ORACLE =====");
+            System.out.println("1. Sumar");
+            System.out.println("2. Restar");
+            System.out.println("3. Multiplicar");
+            System.out.println("4. Dividir");
+            System.out.println("0. Salir");
+
+            System.out.print("seleccione opcion: ");
+            opcion = sc.nextInt();
+
+            if (opcion >= 1 && opcion <= 4) {
+
+                System.out.print("ingrese numero 1: ");
+                double n1 = sc.nextDouble();
+
+                System.out.print("ingrese numero 2: ");
+                double n2 = sc.nextDouble();
+
+                double resultado = 0;
+
+                switch (opcion) {
+                    case 1 -> resultado = calculadora.sumar(n1, n2);
+                    case 2 -> resultado = calculadora.restar(n1, n2);
+                    case 3 -> resultado = calculadora.multiplicar(n1, n2);
+                    case 4 -> resultado = calculadora.dividir(n1, n2);
+                }
+
+                System.out.println("Resultado: " + resultado);
+            }
+        } while (opcion != 0);
+
+        System.out.println("programa finalizado .");
     }
-}
+}          
