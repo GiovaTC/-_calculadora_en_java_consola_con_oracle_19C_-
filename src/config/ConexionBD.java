af1153e077d1e98b484a1943a3d162c86a72eace
gtapiero <giovanny.tapiero@pi.edu.co>
2026-03-05 14:45:44 -0500

    commit - ConexionBD .

--- src/config/ConexionBD.java
+++ src/config/ConexionBD.java
@@ -1,4 +1,21 @@
 package config;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class ConexionBD {
+
+    private static final String URL = "jdbc:oracle:thin:@//localhost:1521/orcl";
+    private static final String USER = "system";
+    private static final String PASSWORD = "Tapiero123";
+
+    public static Connection conectar() {
+
+        try {
+            return DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (Exception e) {
+            System.out.println("error de conexion: "+ e.getMessage());
+            return null;
+        }
+    }
 }
