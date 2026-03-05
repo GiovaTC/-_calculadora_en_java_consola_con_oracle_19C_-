33fa5a05530ab6868d915ae30a25a4731a4e84f8
gtapiero <giovanny.tapiero@pi.edu.co>
2026-03-05 15:00:58 -0500

    commit - CalculadoraServicio .

--- src/servicio/CalculadoraServicio.java
+++ src/servicio/CalculadoraServicio.java
@@ -1,4 +1,37 @@
 package servicio;
 
+import modelo.Operacion;
+import dao.OperacionDAO;
+
 public class CalculadoraServicio {
+
+    private OperacionDAO dao = new OperacionDAO();
+
+    public double sumar(double a, double b) {
+
+        double resultado = a + b;
+        dao.guardarOperacion(new Operacion(a, b, "SUMA", resultado));
+        return resultado;
+    }
+
+    public double restar(double a, double b) {
+
+        double resultado = a - b;
+        dao.guardarOperacion(new Operacion(a, b, "RESTA", resultado));
+        return resultado;
+    }
+
+    public double multiplicar(double a, double b) {
+
+        double resultado = a * b;
+        dao.guardarOperacion(new Operacion(a, b, "MULTIPLICACION", resultado));
+        return resultado;
+    }
+
+    public double dividir(double a, double b) {
+
+        double resultado = a / b;
+        dao.guardarOperacion(new Operacion(a, b, "DIVISION", resultado));
+        return resultado;
+    }
 }
